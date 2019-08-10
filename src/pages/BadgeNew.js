e9e12c41f6a31526727c92a473a6d59d85d03902
Jorge Sánchez <xurxosf@gmail.com>
2019-08-10 23:24:24 +0200

    Implements Create Badge from BadgeForm

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-import './styles/BadgeNew.css';
+import "./styles/BadgeNew.css";
 import logo from "../images/platziconf-logo.svg";
 import Badge from "../components/Badge";
 import BadgeForm from "../components/BadgeForm";
+import api from "../api";
 
 class BadgeNew extends React.Component {
   state = {
@@ -25,21 +26,37 @@ class BadgeNew extends React.Component {
     });
   };
 
+  handleSubmit = async e => {
+    e.preventDefault();
+    this.setState = { loading: true, error: null };
+
+    try {
+      await api.badges.create(this.state.form);
+      this.setState = { loading: false };
+    } catch (error) {
+      this.setState = { loading: false, errir: error };
+    }
+  };
+
   render() {
     return (
       <React.Fragment>
         <div className="BadgeNew__hero">
-          <img className="BadgeNew__hero-image img-fluid" src={logo} alt="Logo" />
+          <img
+            className="BadgeNew__hero-image img-fluid"
+            src={logo}
+            alt="Logo"
+          />
         </div>
 
         <div className="container">
           <div className="row">
             <div className="col-6">
               <Badge
-                firstName={this.state.form.firstName  || 'FIRST_NAME'}
-                lastName={this.state.form.lastName || 'LAST_NAME'}
-                twitter={this.state.form.twitter || 'twitter'}
-                jobTitle={this.state.form.jobTitle || 'JOB_TITLE'}
+                firstName={this.state.form.firstName || "FIRST_NAME"}
+                lastName={this.state.form.lastName || "LAST_NAME"}
+                twitter={this.state.form.twitter || "twitter"}
+                jobTitle={this.state.form.jobTitle || "JOB_TITLE"}
                 email={this.state.form.email}
                 avatarUrl="https://pbs.twimg.com/profile_images/1151113544362078209/chgA6VO9_400x400.jpg"
               />
@@ -47,6 +64,7 @@ class BadgeNew extends React.Component {
             <div className="col-6">
               <BadgeForm
                 onChange={this.handleChange}
+                onSubmit={this.handleSubmit}
                 formValues={this.state.form}
               />
             </div>
